54d878ddfbfe2ad6ea58a69eb0b9a21e82ea4082
Abhishekh <3avu.yd@gmail.com>
2022-09-07 17:43:28 +0530

    auth

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
 const blogModel = require("../Models/blogModel");
 
+
 const authenticate = function (req, res, next) {
   try {
     let token = req.headers["x-api-key"];
-    console.log(req.headers);
     if (!token){
       return res
         .status(404)
@@ -20,30 +20,34 @@ const authenticate = function (req, res, next) {
     });
     next();
   } catch (err) {
-    return res.status(500).send({ msg: "Server Error" });
+    return res.status(500).send({status:false, msg: "Server Error" });
   }
 };
+
+
 
 const authorization = async function (req, res, next) {
   try {
 
-    let id = req.params.blogId;
-    let tokens_Id = req.decodedToken.userId;
+    let idOfBlog = req.params.blogId;
+    let tokensId = req.decodedToken.userId;
 
-    let idOfUser = await blogModel.findById(id);
-    if (!idOfUser)
+    let blogDetails = await blogModel.findById(idOfBlog);
+    if (!blogDetails)
       return res
         .status(404)
         .send({ status: false, msg: "Blog not available" });
-    if (tokens_Id != idOfUser["authorId"].toString()){
+    if (tokensId != blogDetails["authorId"].toString()){
         return res
         .status(404)
         .send({ status: false, msg: "Unknown user not authorized" });
     }
     next();
   } catch (err) {
-    return res.status(500).send({ msg: "Server Error" });
+    return res.status(500).send({status: false, msg: "Server Error" });
   }
 };
+
+
 
 module.exports = { authenticate,  authorization };
