653c88794553ff1f14585c0e6d736c276dc6025b
Abhishekh <3avu.yd@gmail.com>
2022-09-08 18:04:14 +0530

    authDone

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -25,32 +25,5 @@ const authenticate = function (req, res, next) {
   }
 };
 
-const authorization = async function (req, res, next) {
-  try {
-    let pathBlogId = req.params.blogId;
-    let queryDetails = req.query;
-    let queryAuthorId = req.query.authorId;
-    let tokensId = req.decodedToken.userId;
-    let detailsofquery={...queryDetails}
-    let blogDetails = await blogModel.find({
-      $or: [{ _id: pathBlogId }, detailsofquery]
-    });
-    if (!blogDetails)
-      return res.status(404).send({ status: false, msg: "Blog not available" });
 
-
-    for (let i = 0; i < blogDetails.length; i++) {
-      
-      if ( tokensId != blogDetails[i]["authorId"].toString() || tokensId != blogDetails[i]["authorId"].toString() ) {
-        return res
-          .status(404)
-          .send({ status: false, msg: "Unknown user not authorized" });
-      }
-      next();
-    }  
-  } catch (err) {
-    return res.status(500).send({ status: false, msg: err.message });
-  }
-};
-
-module.exports = { authenticate, authorization };
+module.exports = { authenticate };
