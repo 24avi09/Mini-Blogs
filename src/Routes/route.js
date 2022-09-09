2135c569bb8368ddae8aca55f859c6258a121329
Abhishekh <3avu.yd@gmail.com>
2022-09-09 12:40:21 +0530

    alldone

--- src/Routes/route.js
+++ src/Routes/route.js
@@ -3,12 +3,13 @@ const router= express.Router();
 const {createBlog, getBlogs, updateBlogs,deleteBlog,deletedocs, loginUser}= require("../Controller/blogController");
 const {createAuthor}= require("../Controller/authorController");
 const {authenticate}= require("../Middleware/auth");
+const {authorValidation , blogsValidation }= require("../validator/validator");
 
 
 
-router.post("/authors", createAuthor)// API for Creation of Author 
+router.post("/authors", authorValidation , createAuthor)// API for Creation of Author 
 
-router.post("/blogs",authenticate, createBlog)// API for Creation of blogs
+router.post("/blogs", blogsValidation , authenticate, createBlog)// API for Creation of blogs
 
 //
 
@@ -16,10 +17,10 @@ router.get("/blogs",authenticate, getBlogs)// API for getting of blogs
 
 router.put("/blogs/:blogId",authenticate, updateBlogs)// API for updating of blogs 
 
-router.delete("/blogs/:blogId",authenticate, deleteBlog)
+router.delete("/blogs/:blogId",authenticate, deleteBlog)// API for deleting blog by path Params
 
-router.delete("/blogs",authenticate, deletedocs)
+router.delete("/blogs",authenticate, deletedocs)// API for deleting blog by query Params
 
-router.post("/login", loginUser )
+router.post("/login", loginUser )// API for login author
 
 module.exports= router;
